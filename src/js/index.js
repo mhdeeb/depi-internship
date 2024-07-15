fbdbd39a67a0ff0e6dfd949a02505b7a1faef8fa
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-07-16 00:35:28 +0300

    Update image loading

--- src/js/index.js
+++ src/js/index.js
@@ -3,3 +3,17 @@ console.log("JavaScript loaded successfully!");
 function getStartedButton() {
   console.log("Getting started button clicked!");
 }
+
+document.addEventListener("DOMContentLoaded", function (e) {
+  const progressiveImages = document.getElementsByClassName(
+    "progressive-replace"
+  );
+
+  for (const progressiveImage of progressiveImages) {
+    progressiveImage.addEventListener("load", () => {
+      const target = progressiveImage.nextElementSibling;
+      target.src = progressiveImage.src;
+      target.classList.add("progressive-reveal");
+    });
+  }
+});
