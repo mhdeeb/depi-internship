401549854d24d32779102e094eb0542a7d8185da
Mohamed Hussien El-Deeb <s-mohamed.eldeeb@zewailcity.edu.eg>
2024-07-16 21:28:29 +0300

    Update scroll behavior

--- src/js/index.js
+++ src/js/index.js
@@ -18,3 +18,23 @@ document.addEventListener("DOMContentLoaded", function (e) {
       });
   });
 });
+
+const debounce = (fn) => {
+  let frame;
+  return (...params) => {
+    if (frame) {
+      cancelAnimationFrame(frame);
+    }
+    frame = requestAnimationFrame(() => {
+      fn(...params);
+    });
+  };
+};
+
+const storeScroll = () => {
+  document.documentElement.dataset.scroll = window.scrollY;
+};
+
+document.addEventListener("scroll", debounce(storeScroll), { passive: true });
+
+storeScroll();
