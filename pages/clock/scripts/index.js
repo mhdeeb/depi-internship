d517ec7e43973398e8ac5ccbe05564308f2b79cf
mhdeeb <mhdeeb@users.noreply.github.com>
2024-08-14 13:24:56 +0000

    deploy: 471f727bdcc288f1a5b7fe5b21bc98adf4d88ba0

---
+++ pages/clock/scripts/index.js
@@ -0,0 +1,43 @@
+let clock = document.querySelector(".clock");
+let digital = document.querySelector(".digital-clock");
+
+function updateClock() {
+  let now = new Date();
+
+  clock.style.setProperty(
+    "--hour",
+    now.getHours() + now.getMinutes() / 60 + now.getSeconds() / 3600
+  );
+  clock.style.setProperty("--minute", now.getMinutes() + now.getSeconds() / 60);
+  clock.style.setProperty("--second", now.getSeconds());
+
+  digital.textContent = now.toLocaleTimeString();
+
+  setTimeout(updateClock, 1000);
+}
+
+function setNumbers() {
+  let numbers = clock.querySelector(".text");
+  let bticks = clock.querySelector(".bticks");
+
+  const li = document.createElement("li");
+
+  for (let i = 1; i <= 12; i++) {
+    let number = li.cloneNode();
+    number.style.setProperty("--unit", i);
+    numbers.appendChild(number);
+    bticks.appendChild(number.cloneNode());
+  }
+
+  let ticks = clock.querySelector(".ticks");
+
+  for (let i = 1; i <= 60; i++) {
+    let number = li.cloneNode();
+    number.style.setProperty("--unit", i);
+    ticks.appendChild(number);
+  }
+}
+
+setNumbers();
+
+updateClock();
